ede3ddaa6d6ed1ef9862702fe6f4edc4022a1ac3
tuhin-ghorui <tuhinghorui553@gmail.com>
2026-05-21 09:41:16 +0530

    Learned about the printing and taking user i nput in python

--- phase-1-foundation/month-01-python-basics/exercises/hello.py
+++ phase-1-foundation/month-01-python-basics/exercises/hello.py
@@ -1,1 +1,8 @@
+#printing message
 print("Hello, World")
+
+#Taking user input and storing it into the varible
+name = input("What's your name? ")
+
+#String concentration in python
+print("Hello,", name)
