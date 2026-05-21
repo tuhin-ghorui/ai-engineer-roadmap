fa1712a6d11fe30cd11ae5a9a464d541f3b0a82e
tuhin-ghorui <tuhinghorui553@gmail.com>
2026-05-21 11:25:54 +0530

    code added

--- phase-1-foundation/month-01-python-basics/exercises/hello.py
+++ phase-1-foundation/month-01-python-basics/exercises/hello.py
@@ -3,6 +3,18 @@ print("Hello, World")
 
 #Taking user input and storing it into the varible
 name = input("What's your name? ")
+#Output = What's your name? Tuhin
+#Hello, Tuhin
 
 #String concentration in python
 print("Hello,", name)
+
+#using end=" " to end the string with space
+print("Hello,", end="$$$")
+print(name)
+#Output= Hello,$$$Tuhin
+
+#sep='' is used to seperate 2 different variable or string
+print("Hello,", name, sep='???')
+print(name)
+#Output = Hello,???Tuhin
